ff4826ca9f927291852e8d7e3175f2b082ca47d7
Donny <zombieminer105@gmail.com>
2022-01-06 21:00:26 -0500

    Restore script necessary for player name displaying that was somehow deleted.

---
+++ Assets/Scripts/PlayerDisplayer.cs
@@ -0,0 +1,39 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDisplayer : NetworkBehaviour
+{
+    [SyncVar(hook = "DisplayName")] public string playerName;
+
+    private HealthUI ui;
+    void Start()
+    {
+        ui = FindObjectOfType<HealthUI>();
+        if (isLocalPlayer && string.IsNullOrEmpty(playerName))
+        {
+            // Send the name to the server and then display to all clients if it is the local player's first time running Start()
+            CmdSendName(PlayerPrefs.GetString("PlayerName"));
+        } 
+        else if(!isLocalPlayer && !string.IsNullOrEmpty(playerName))
+        {
+            // Display the name already set to the server if client joining later
+            DisplayName(playerName, playerName);
+        }
+    }
+    [Command]
+    public void CmdSendName(string name)
+    {
+        playerName = name;
+    }
+    public void DisplayName(string oldName, string newName)
+    {
+        Debug.Log("Displaying name.");
+        ui.AddPlayerUI(gameObject, playerName);
+    }
+    public override void OnStopClient()
+    {
+        ui.RemovePlayerUI(gameObject);
+    }
+}
