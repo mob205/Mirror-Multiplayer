d324aef7ca47bba077570db365a70a5c06e6221e
Donny <zombieminer105@gmail.com>
2022-04-01 19:41:06 -0400

    Add item spawner uses weighted random selection for each item instead of each item having equal chance of selection.

--- Assets/Scripts/Game/ItemSpawner.cs
+++ Assets/Scripts/Game/ItemSpawner.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class ItemSpawner : NetworkBehaviour
 {
     [SerializeField] private Transform[] spawnLocations;
-    [SerializeField] private GameObject[] spawnableItems;
+    [SerializeField] private Spawnable[] spawnableItems;
     [SerializeField] private int totalSpawns;
 
     public override void OnStartServer()
@@ -18,13 +18,34 @@ public class ItemSpawner : NetworkBehaviour
             {
                 // Spawn a random item at a random location. 
                 var locIndex = Random.Range(0, locations.Count);
-                var itemIndex = Random.Range(0, spawnableItems.Length);
-                var spawnedItem = Instantiate(spawnableItems[itemIndex], locations[locIndex].position, Quaternion.identity);
-                NetworkServer.Spawn(spawnedItem);
+                var spawnedItem = Instantiate(GetRandomItem(), locations[locIndex].position, Quaternion.identity);
+                NetworkServer.Spawn(spawnedItem.gameObject);
 
                 // One location should not have two items.
                 locations.Remove(locations[locIndex]);
             }
         }
     }
+    private Item GetRandomItem()
+    {
+        var totalWeight = spawnableItems.Sum(p => p.weight);
+        var randomWeight = Random.Range(0, totalWeight) + 1;
+        foreach(var spawnable in spawnableItems)
+        {
+            randomWeight -= spawnable.weight;
+            if(randomWeight <= 0)
+            {
+                return spawnable.item;
+            }
+        }
+        Debug.LogError("Item was not randomly selected. Selecting last item.");
+        return spawnableItems[spawnableItems.Length - 1].item;
+    }
 }
+[System.Serializable]
+public struct Spawnable 
+{
+    [SerializeField] public Item item;
+    [SerializeField] public int weight;
+}
+
