a92c820e11782cf274fefdc9af8d78c913ee90f1
Donny <zombieminer105@gmail.com>
2022-04-17 12:55:40 -0400

    Add charge-up to Explosion ability. Change explosion particles to stop on walls to reflect the line-of-sight check in damage calculation.

--- Assets/Scripts/Abilities & Upgrades/Abilities/ExplosionAbility.cs
+++ Assets/Scripts/Abilities & Upgrades/Abilities/ExplosionAbility.cs
@@ -5,15 +5,49 @@ using System.Linq;
 
 public class ExplosionAbility : AbilityUpgrade
 {
-    public ParticleSystem particles;
+    [Header("General")]
+    public ParticleSystem expParticlePrefab;
+    public ParticleSystem startParticlePrefab;
     public LayerMask wallLayers;
+    [Header("Gameplay")]
     public float range;
     public float damage;
+    public float warmupDuration;
     public override void CastAbility(Vector2 mousePos)
     {
+        StartCoroutine(DelayedCalculateDamage());
+        StartCooldown();
+    }
+    public override void ClientCastAbility(Vector2 mousePos)
+    {
+        StartCoroutine(PlayEffects());
+        base.ClientCastAbility(mousePos);
+    }
+    private IEnumerator PlayEffects()
+    {
+        PlayChargeParticles();
+        yield return new WaitForSeconds(warmupDuration);
+        Instantiate(expParticlePrefab, transform.position, Quaternion.identity);
+    }
+    private void PlayChargeParticles()
+    {
+        var startParticles = Instantiate(startParticlePrefab, transform.position, Quaternion.identity, transform);
+        var startMain = startParticles.main;
+
+        // time = distance / speed 
+        var lifetime = startParticles.shape.radius / Mathf.Abs(startMain.startSpeed.constant);
+        startMain.startLifetime = lifetime;
+
+        // warmupDuration (total playtime of system) = duration of spawning particles + lifetime of lingering particles
+        startMain.duration = warmupDuration - lifetime;
+        startParticles.Play();
+    }
+    private IEnumerator DelayedCalculateDamage()
+    {
+        yield return new WaitForSeconds(warmupDuration);
         var self = GetComponent<Health>();
         var targetsInRange = Physics2D.OverlapCircleAll(transform.position, range);
-        foreach(var target in targetsInRange)
+        foreach (var target in targetsInRange)
         {
             var targetHealth = target.GetComponent<Health>();
             if (targetHealth && targetHealth != self && !Physics2D.Linecast(transform.position, target.transform.position, wallLayers))
@@ -21,11 +55,5 @@ public class ExplosionAbility : AbilityUpgrade
                 targetHealth.Damage(damage, gameObject);
             }
         }
-        StartCooldown();
-    }
-    public override void ClientCastAbility(Vector2 mousePos)
-    {
-        Instantiate(particles, transform.position, Quaternion.identity);
-        base.ClientCastAbility(mousePos);
     }
 }
